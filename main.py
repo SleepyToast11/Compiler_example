6f241d429d458b2439eeb9a67d1d63a96f4ddbc9
SleepyToast11 <jesp2000@live.ca>
2024-05-29 22:20:43 -0400

    Add config file and modify main to use it instead of sys args

--- main.py
+++ main.py
@@ -6,13 +6,33 @@ import sys
 def main():
     # Get the command from the file given and move it
     # to the file the rover is watching (default is Rover1)
-    rover_name = "Rover1"
-    if len(sys.argv) < 1:
-        raise Exception("Missing file path to parse.")
 
+
+
+    rover_name = "Rover1"
+
+    config_file_path = pathlib.Path("config")
+
+    try:
+        config_file_fd = open(config_file_path, "r")
+        try:
+            rover_communication_file_path = config_file_fd.readline()
+        except:
+            print("failed to read config file")
+
+    except:
+        config_file_fd = open(config_file_path, "w+")
+        config_file_fd.write("Rover.txt")
+        config_file_fd.close()
+
+        config_file_fd = open(config_file_path, "r")
+        rover_communication_file_path = config_file_fd.readline()
+
+    finally:
+        config_file_fd.close()
 
     fcontent = None
-    filepath = pathlib.Path(sys.argv[1])
+    filepath = pathlib.Path(rover_communication_file_path)
     with filepath.open() as f:
         fcontent = f.read()
 
